eec618863106639730e1d070cc7f8d345fa21781
fehim <fehimornek@gmail.com>
2022-03-22 11:45:03 +0300

    added decrease button

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,11 +10,18 @@ const Header = () => {
   const reset=()=>{
     setCount(count => 0)
   }
+  const decrease=()=>{
+    setCount(count => count - 1)
+  }
+
   return (
     <div className='main_container'>
       <h1>Button Increment Example</h1>
-      <button onClick={()=>increment()} className="btn">Inc</button>
-      <button onClick={()=>reset()} className="btn">Reset</button>
+      <div className='button_container'>
+        <button onClick={()=>increment()} className="btn">+</button>
+        <button onClick={()=>reset()} className="btn">C</button>
+        <button onClick={()=>decrease()} className="btn">-</button>
+      </div>
       <h3>Count: {count}</h3>
     </div>
   )
