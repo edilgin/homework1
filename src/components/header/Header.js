dea6b384833d5f5f0e6fdfa468a8ca5f4ce83f6c
fehim <fehimornek@gmail.com>
2022-03-22 11:32:02 +0300

    added reset button

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,15 +3,18 @@ import {useState} from "react";
 import "./header.css"
 
 const Header = () => {
-  let c = 0
   const [count, setCount] = useState(0)
   const increment=()=>{
     setCount(count => count + 1)
   }
+  const reset=()=>{
+    setCount(count => 0)
+  }
   return (
     <div className='main_container'>
       <h1>Button Increment Example</h1>
-      <button onClick={()=>increment()} className="btn">Click</button>
+      <button onClick={()=>increment()} className="btn">Inc</button>
+      <button onClick={()=>reset()} className="btn">Reset</button>
       <h3>Count: {count}</h3>
     </div>
   )
